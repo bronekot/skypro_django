8876ab2682ca38014247dc68dcd60f7a8b9bb068
BroneKot <13498924+bronekot@users.noreply.github.com>
2024-09-15 00:48:55 +0300

    migrate

--- catalog/models.py
+++ catalog/models.py
@@ -15,11 +15,15 @@ class Product(models.Model):
     Методы:
         __str__(): Возвращает строковое представление продукта.
     """
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.ImageField(upload_to="products/")
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to="products/", verbose_name="Изображение")
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE, verbose_name="Категория"
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    manufactured_at = models.DateField(verbose_name="Дата производства", null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -38,11 +42,11 @@ class Category(models.Model):
     Методы:
         __str__(): Возвращает строковое представление категории.
     """
-    name = models.CharField(max_length=255)
-    description = models.TextField()
+
+    name = models.CharField(max_length=255, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.name)
-
