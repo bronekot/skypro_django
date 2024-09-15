b4f7ba5fb38e3082f3024c1dba7b85205c466b7c
BroneKot <13498924+bronekot@users.noreply.github.com>
2024-09-15 07:01:11 +0300

    final

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    print(f"Products: {products}")
+    context = {"object_list": products}
+    return render(request, "home.html", context)
 
 
 def contacts(request):
@@ -14,3 +18,9 @@ def contacts(request):
         print(name, phone, message)
 
     return render(request, "contacts.html")
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"object": product}
+    return render(request, "product_detail.html", context)
